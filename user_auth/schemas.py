9cda7a9823ff2ecc73f6cd2b357710ce1d7ede98
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-09-04 17:32:08 +0300

    Added user auth app with create user schema in schemas.py file

---
+++ user_auth/schemas.py
@@ -0,0 +1,21 @@
+import string
+from pydantic import BaseModel, EmailStr, Field, field_validator
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    username: str = Field(min_length=5, max_length=20)
+    password: str = Field(min_length=8)
+
+    @field_validator("password")
+    def validate_password_startswith_upper(cls, value: str) -> str:
+        if not value[0].isupper():
+            raise ValueError("Password must start with an uppercase letter")
+        return value
+
+    @field_validator("username")
+    def validate_username_with_ascii_letters(cls, value: str) -> str:
+        for i in range(len(value)):
+            if value[i] not in string.ascii_letters:
+                raise ValueError("Username must contain only ASCII letters")
+        return value
