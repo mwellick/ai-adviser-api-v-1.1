808d61cd740d6236f7a77ba557edfe5d455bece5
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-09-05 21:50:43 +0300

    Added chat app with chat create & chats list schema

---
+++ chat/schemas.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class ChatCreate(BaseModel):
+    theme_id: int
+    user_id: Optional[int] = None  # user or guest
+
+
+class ChatRead(BaseModel):
+    id: int
+    created_at: datetime
+    theme_id: int
+    user_id: Optional[int] = None  # user or guest
+    is_saved: bool
