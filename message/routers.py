02415f0031fa21fd9f1ec64a046877da1509ffb1
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-09-12 13:30:34 +0300

    Added message create router

--- message/routers.py
+++ message/routers.py
@@ -1,13 +1,23 @@
+import os
 from fastapi import APIRouter, Path
+from openai import AsyncOpenAI
 from dependencies import db_dependency
 from starlette import status
-from database.models import Message
+
+from .crud import message_create
 from .schemas import MessageCreate
+from dotenv import load_dotenv
+
+load_dotenv()
 
 messages_router = APIRouter(
     prefix="/chat",
     tags=["messages"]
 )
+
+OPEN_AI_KEY = os.environ.get("OPENAI_API_KEY")
+
+client = AsyncOpenAI(api_key=OPEN_AI_KEY)
 
 
 @messages_router.post("/{chat_id}/message", status_code=status.HTTP_201_CREATED)
@@ -16,11 +26,6 @@ async def create_message(
         message: MessageCreate,
         chat_id: int = Path(gt=0)
 ):
-    message = Message(
-        content=message.content,
-        chat_id=chat_id
-    )
+    response = await message_create(db, message, chat_id)
 
-    db.add(message)
-    await db.commit()
-    return message
+    return response
