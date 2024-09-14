4d4c60a45b1a13800cf2ae52ac38014fa36e3ddd
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-09-14 15:02:17 +0300

    Implemented  new endpoint for guest chat messaging

--- message/routers.py
+++ message/routers.py
@@ -8,43 +8,29 @@ from .crud import message_create
 from .schemas import MessageCreate
 from dotenv import load_dotenv
 
-from .utils import generate_guest_response
+from .utils import generate_guest_response, set_cookie, get_cookie
 
 load_dotenv()
 
 messages_router = APIRouter(
-    prefix="/chat",
     tags=["messages"]
 )
 
 MAX_MESSAGES = 10
 
 
-@messages_router.post("/{chat_id}/message")
-async def create_message(
-        db: db_dependency,
+@messages_router.post("guest_chat/{chat_id}/message")
+async def create_message_by_guest(
         request: Request,
         response: Response,
-        message: MessageCreate,
-        chat_id: int = Path(gt=0)
+        message: MessageCreate
 ):
-    chat_data_cookies = request.cookies.get("guest_chat_data")
-    cookie_chat_id = ""
-    if chat_data_cookies is not None:
-        chat_data = json.loads(chat_data_cookies)
-        cookie_chat_id = chat_data.get("chat_id")
+    chat_data_cookies = await get_cookie(request, "guest_chat_data")
 
-    if cookie_chat_id is None:
-        response = await message_create(db, message, chat_id)
-    else:
-        chat_data_cookies = request.cookies.get("guest_chat_data")
-        if chat_data_cookies:
-            chat_data = json.loads(chat_data_cookies)
-            chat_id = chat_data.get("chat_id")
-            theme_id = chat_data.get("theme_id")
+    chat_id = chat_data_cookies.get("chat_id")
+    theme_id = chat_data_cookies.get("theme_id")
 
-    messages_cookies = request.cookies.get("guest_messages")
-    messages = json.loads(messages_cookies) if messages_cookies else []
+    messages = await get_cookie(request, "guest_messages")
 
     messages.append(
         {
@@ -53,15 +39,20 @@ async def create_message(
             "theme_id": theme_id
         }
     )
-    response.set_cookie(
-        key="guest_messages",
-        value=json.dumps(messages),
-        max_age=0,
-        httponly=True,
-        secure=True
-    )
+    await set_cookie(response, "guest_messages", messages, max_age=1800)
 
     ai_response = await generate_guest_response(messages)
     response_message = {"response": ai_response}
 
     return response_message
+
+
+@messages_router.post("user_chat/{chat_id}/message")
+async def create_message(
+        db: db_dependency,
+        message: MessageCreate,
+        chat_id: int = Path(gt=0)
+):
+    response = await message_create(db, message, chat_id)
+
+    return response
