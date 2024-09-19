0b4b3ea0eefa9f986caae35d5f3a3d7d41798e84
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-09-19 20:44:56 +0300

    Added temporary endpoints to add  and delete themes

--- theme/routers.py
+++ theme/routers.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
+from sqlalchemy import select
 from starlette import status
 from dependencies import db_dependency, user_dependency
 from .crud import get_themes_list
@@ -20,8 +21,34 @@ async def get_all_themes(db: db_dependency):
     return await get_themes_list(db)
 
 
-@themes_router.post("/create")
-async def create_theme(db: db_dependency, user: user_dependency):
+@themes_router.post("/create", status_code=status.HTTP_201_CREATED)
+async def create_theme(
+        name: str,
+        description: str,
+        db: db_dependency,
+        user: user_dependency
+):  # TODO Temporary endpoint. To delete it when project goes to prod
     theme = Theme(
+        name=name,
+        description=description
     )
-    ...
+    db.add(theme)
+    await db.commit()
+
+
+@themes_router.delete("/{theme_id}/delete", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_theme(
+        db: db_dependency,
+        user: user_dependency,
+        theme_id: int = Path(gt=0)
+):  # TODO Temporary endpoint. To delete it when project goes to prod
+    query = select(Theme).where(
+        Theme.id == theme_id)
+
+    result = await db.execute(query)
+
+    theme_to_delete = result.scalars().first()
+
+    await db.delete(theme_to_delete)
+    await db.commit()
+    return None
